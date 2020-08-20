6afb45fb999621b897a180d0c35aa8bd338f71bb
Hirok Kangsha Banik <hirokbanik@gmail.com>
2020-08-20 11:56:45 +0530

    routify-tailwind-starter

---
+++ postcss.config.js
@@ -0,0 +1,24 @@
+// postcss.config.js
+
+const cssnano = require('cssnano')({preset: 'default'})
+
+const purgecss = require('@fullhuman/postcss-purgecss')({
+    content: [
+      './**/**/*.html',
+      './**/**/*.svelte',
+    ],
+
+    whitelistPatterns: [/svelte-/],
+
+    defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
+  });
+
+const production = !process.env.ROLLUP_WATCH
+
+module.exports = {
+  plugins: [
+    require('tailwindcss'),
+    require('autoprefixer'),
+    ...(production ? [purgecss, cssnano] : [])
+  ]
+};
---
+++ src/App.svelte
@@ -0,0 +1,19 @@
+<script>
+  import { Router } from "@sveltech/routify";
+  import { routes } from "../.routify/routes";
+</script>
+
+<style  global>
+  /* purgecss start ignore */
+  @import 'tailwindcss/base';
+
+  @import 'tailwindcss/components';
+  /* purgecss end ignore */
+
+  @import 'tailwindcss/utilities';
+
+  /* Don't forget to remove unused styles in global.css */
+  @import "../static/global.css";
+</style>
+
+<Router {routes} />
